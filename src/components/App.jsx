86d6136245d96cdd7742020a42d8b9306c10df55
LiudmylaBolotova <liudmylabolotova@gmail.com>
2024-02-11 15:40:32 +0200

    removing unnecessary files

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,10 @@
 import React from 'react';
 import Gradient from 'components/Gradient';
-import Conversion from 'components/Conversion';
 
 export const App = () => {
   return (
     <>
       <Gradient />
-      <Conversion/>
     </>
   );
 };
