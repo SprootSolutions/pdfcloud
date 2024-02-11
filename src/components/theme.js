9ad54e904ee5065cfdedcf0bc21a6d0a361cbac4
LiudmylaBolotova <liudmylabolotova@gmail.com>
2024-02-11 03:27:29 +0200

    Added svg for loading, Conversion component, styles for it

--- src/components/theme.js
+++ src/components/theme.js
@@ -1,15 +1,16 @@
 export const theme = {
   colors: {
-    text: '#00bfff',
-    textLogo: '#FFFFFF',
+    textPDF: '#636363',
     accent: '#08ffff',
-    spanLogo: '#FF0000',
-    backgroundColor: '#E5E4E2',
+    backgroundColorBtn: '#FFFFFF',
   },
 
   sizes: {
-    width: [5, 10, 20, 30, 40, 50, 60, 70, 80, 100, 150, 200, 300, 400, 1440],
-    height: [5, 10, 20, 30, 40, 50, 60, 70, 80, 100, 150, 300, 350, 800],
+    width: [
+      5, 10, 20, 30, 40, 50, 60, 70, 80, 100, 150, 166, 200, 300, 400, 500,
+      1440,
+    ],
+    height: [5, 10, 20, 30, 40, 50, 60, 70, 77, 80, 100, 150, 300, 350, 800],
   },
 
   fontSizes: [10, 12, 16, 18, 20, 32],
@@ -20,15 +21,15 @@ export const theme = {
     bold: 700,
   },
 
-  space: [0, 2, 4, 8, 16, 20, 25, 30, 40],
+  space: [0, 2, 4, 8, 10, 20, 25, 30, 40],
 
   borders: {
     small: '1px solid #9B9B9B',
-    smallBtn: '1px solid #FFFFFF'
+    smallBtn: '1px solid #FFFFFF',
   },
 
   radii: {
-    round: 20,
+    round: 10,
     btnRound: 30,
   },
 };
